6f96164adb12af9232babe54aa64c6a6d2073940
Haeyoung <97646668+drsuneamer@users.noreply.github.com>
2024-04-06 04:45:28 +0900

    Time: 2 ms (88.29%), Space: 42.4 MB (31.14%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,27 +1,20 @@
 class Solution {
-    
-    int check(int idx, String ss) {
-        for (int i = idx; i < ss.length() - 1; i++) {
+    String check(String ss) {
+        for (int i = 0; i < ss.length() - 1; i++) {
             if (Math.abs(ss.charAt(i) - ss.charAt(i + 1)) == 32) {
-                // 아스키값 차이가 32인 것 -> 대소문자 인 관계 발견하면 둘 다 삭제
-                return i;
+                return ss.substring(0, i) + ss.substring(i + 2);
             }
         }
-        return -1;
+        return "end";
     }
     
     public String makeGood(String s) {
-        int idx = 0;
-
         while (true) {
-            // 모두 없어질때까지 반복
-            idx = check(idx, s);
-            if (idx == -1) {
+            String re = check(s);
+            if (re == "end") {
                 break;
             }
-            s = s.substring(0, idx) + s.substring(idx + 2);
-            idx = Math.max(0, idx - 2);  // 인덱스 에러 방지
-
+            s = re;
         }
         return s;
     }
