676ad9b276b6250b76d395d92e16b743acf7fcea
Haeyoung <97646668+drsuneamer@users.noreply.github.com>
2024-04-06 03:58:34 +0900

    Time: 2 ms (88.33%), Space: 43.1 MB (12.07%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    String check(String ss) {
+        for (int i = 0; i < ss.length() - 1; i++) {
+            if (Math.abs(ss.charAt(i) - ss.charAt(i + 1)) == 32) {
+                return ss.substring(0, i) + ss.substring(i + 2);
+            }
+        }
+        return "end";
+    }
+    
+    public String makeGood(String s) {
+        while (true) {
+            String re = check(s);
+            if (re == "end") {
+                break;
+            }
+            s = re;
+        }
+        return s;
+    }
+}
